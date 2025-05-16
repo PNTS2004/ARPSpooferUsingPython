74674b4540103ff0537260dafa1ff9b52b53b9bd
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-05-16 12:53:38 -0400

    Progess Continued

--- ARPSpooferUsingPython/arpspoofer.py
+++ ARPSpooferUsingPython/arpspoofer.py
@@ -2,7 +2,14 @@ import scapy.all as scapy
 import argparse
 
 def get_mac():
-
+    
 def get_arguments():
+    
 
-def spoof():
+def spoof(target_ip, spoof_ip):
+    target_mac=get_mac(target_ip)
+    packet=scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+
+arguments=get_argumnets()
+spoof(arguments.target, argument.gateway)
+spoof(argument.gateway, arguments.target)
